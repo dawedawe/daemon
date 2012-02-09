be1839cae9d4e2ba86befbea197c7d97264940d8
dawe <dawe@padtree.my.domain>
2012-02-09 04:42:37 +0100

    reformat Daemon.hs

--- Daemon.hs
+++ Daemon.hs
@@ -24,8 +24,8 @@ countAndPrint conf keywords = do
 	let flattened_ts = prepareNewsData ts
 	let counts = countWordsInWords keywords flattened_ts
 	if optVerbose (opts conf) == True
-		then mapM_ (putStrLn . comb') $ zip keywords counts
-		else mapM_ (putStrLn . comb) $ zip keywords (map fst counts)
+	  then mapM_ (putStrLn . comb') $ zip keywords counts
+	  else mapM_ (putStrLn . comb) $ zip keywords (map fst counts)
 
 runTasks :: Conf -> IO ()
 runTasks conf = do
@@ -37,13 +37,13 @@ runTask :: [String] -> Task -> IO ()
 runTask news t = do
 	let count = countWordInWords (keyword t) news
 	if count >= (threshold t)
-		then do
-				putStrLn ("Threshold " ++ (show (threshold t)) ++
-					" exceeded for " ++ (keyword t))
-				_ <- runCommand $ action t
-				return ()
-		else do putStrLn ("Threshold " ++ (show (threshold t)) ++ " for " ++
-				(keyword t) ++ " not exceeded")
+	  then do
+	    putStrLn ("Threshold " ++ (show (threshold t)) ++
+	      " exceeded for " ++ (keyword t))
+	    _ <- runCommand $ action t
+	    return ()
+	  else putStrLn ("Threshold " ++ (show (threshold t)) ++ " for " ++
+	         (keyword t) ++ " not exceeded")
 
 getFeedTitleStrings :: Proxy -> String -> IO [String]
 getFeedTitleStrings prox url = do
@@ -55,13 +55,12 @@ printFeedTitleStrings :: [String] -> IO ()
 printFeedTitleStrings s = mapM_ putStrLn s
 
 getPage :: Proxy -> String -> IO String
-getPage prox url =
-	do
-		(_,rsp) <- Network.Browser.browse $ do
-			setProxy prox
-			setOutHandler $ const (return ()) -- no debug output
-			request (getRequest url)
-		return (rspBody rsp)
+getPage prox url = do
+	(_,rsp) <- Network.Browser.browse $ do
+	  setProxy prox
+	  setOutHandler $ const (return ()) -- no debug output
+	  request (getRequest url)
+	return (rspBody rsp)
 
 prepareNewsData :: [[String]] -> [String]
 prepareNewsData news = map lowerString $ flatten news
@@ -69,39 +68,39 @@ prepareNewsData news = map lowerString $ flatten news
 countWordsInWords :: [String] -> [String] -> [(Int, [T.Text])]
 countWordsInWords swords wrds = countWordsInWords' swords wrds
 	where
-		countWordsInWords' :: [String] -> [String] -> [(Int, [T.Text])]
-		countWordsInWords' [] _ = []
-		countWordsInWords' _ [] = []
-		countWordsInWords' (x:xs) ws =
-			(countWordInWords' x ws) : (countWordsInWords' xs ws)
+	  countWordsInWords' :: [String] -> [String] -> [(Int, [T.Text])]
+	  countWordsInWords' [] _ = []
+	  countWordsInWords' _ [] = []
+	  countWordsInWords' (x:xs) ws =
+	    (countWordInWords' x ws) : (countWordsInWords' xs ws)
 
 countWordInWords :: String -> [String] -> Int
 countWordInWords "" _ = 0
 countWordInWords _ [] = 0
 countWordInWords w ws = helper (T.pack (lowerString w)) 0 (map T.pack ws)
 	where
-		helper :: T.Text -> Int -> [T.Text] -> Int
-		helper _ _ []				= 0
-		helper w' i [(x)]
-			| (T.isInfixOf w' x)	= (i+1)
-			| otherwise				= i
-		helper w' i (x:xs)
-			| (T.isInfixOf w' x)	= helper w' (i+1) xs
-			| otherwise				= helper w' i xs
+	  helper :: T.Text -> Int -> [T.Text] -> Int
+	  helper _ _ []	         = 0
+	  helper w' i [(x)]
+	    | (T.isInfixOf w' x) = (i+1)
+	    | otherwise          = i
+	  helper w' i (x:xs)
+	    | (T.isInfixOf w' x) = helper w' (i+1) xs
+	    | otherwise          = helper w' i xs
 
 countWordInWords' :: String -> [String] -> (Int, [T.Text])
 countWordInWords' "" _ = (0, [])
 countWordInWords' _ [] = (0, [])
 countWordInWords' w ws = helper (T.pack (lowerString w)) (0, []) (map T.pack ws)
 	where
-		helper :: T.Text -> (Int, [T.Text]) -> [T.Text] -> (Int, [T.Text])
-		helper _ _ []				= (0, [])
-		helper w' (i, ins) [(x)]
-			| (T.isInfixOf w' x)	= ((i+1), (x : ins))
-			| otherwise				= (i, ins)
-		helper w' (i, ins) (x:xs)
-			| (T.isInfixOf w' x)	= helper w' ((i+1), (x : ins)) xs
-			| otherwise				= helper w' (i, ins) xs
+	  helper :: T.Text -> (Int, [T.Text]) -> [T.Text] -> (Int, [T.Text])
+	  helper _ _ []          = (0, [])
+	  helper w' (i, ins) [(x)]
+	    | (T.isInfixOf w' x) = ((i+1), (x : ins))
+	    | otherwise		 = (i, ins)
+	  helper w' (i, ins) (x:xs)
+	    | (T.isInfixOf w' x) = helper w' ((i+1), (x : ins)) xs
+	    | otherwise          = helper w' (i, ins) xs
 
 comb :: (String, Int) -> String
 comb (a, b) = a ++ "\t" ++ (show b)
